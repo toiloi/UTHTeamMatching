8a9d57a2a6d2d1c10d70637e308de18b21748391
HPhi1808 <hoangphi120705@gmail.com>
2025-04-07 19:07:20 +0700

    hien thi bai viet

--- src/main/java/org/example/uthteammatching/controllers/homeController.java
+++ src/main/java/org/example/uthteammatching/controllers/homeController.java
@@ -1,26 +1,3 @@
-//package org.example.uthteammatching.controllers;
-//import org.example.uthteammatching.models.BaiViet;
-//import org.example.uthteammatching.models.Project;
-//import org.example.uthteammatching.models.UthUser;
-//import org.example.uthteammatching.services.PostService;
-//import org.example.uthteammatching.services.ProjectService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PathVariable;
-//
-//import java.util.List;
-//
-//@Controller
-//public class homeController {
-//    @GetMapping("/")
-//    public String home(){
-//        return "home";
-//    }
-//
-//}
-
 package org.example.uthteammatching.controllers;
 
 import org.example.uthteammatching.models.Project;

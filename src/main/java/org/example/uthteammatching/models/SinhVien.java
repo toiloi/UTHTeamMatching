761be84af3833b377fca640b2d3ba03fd6593ec6
toiloi <anhtoai847@gmail.com>
2025-04-12 01:02:55 +0700

    hoàn thiện chi tiết sinh viên

--- src/main/java/org/example/uthteammatching/models/SinhVien.java
+++ src/main/java/org/example/uthteammatching/models/SinhVien.java
@@ -3,6 +3,7 @@ package org.example.uthteammatching.models;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.Nationalized;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
@@ -12,16 +13,16 @@ import org.hibernate.annotations.OnDeleteAction;
 @Entity
 public class SinhVien {
     @Id
-    @Nationalized
-    @Column(name = "maSo", nullable = false, length = 50)
-    private Long maSo;
+    @Column(name = "maSo", nullable = false)
+    private Long id;
 
     @MapsId
     @OneToOne(fetch = FetchType.LAZY, optional = false)
     @OnDelete(action = OnDeleteAction.CASCADE)
     @JoinColumn(name = "maSo", nullable = false)
-    private org.example.uthteammatching.models.UthUser uthUser;
+    private UthUser uthUser;
 
+    @ColumnDefault("100")
     @Column(name = "mucDoUyTin")
     private Integer mucDoUyTin;
 
@@ -30,9 +31,11 @@ public class SinhVien {
     @Column(name = "kyNang")
     private String kyNang;
 
+    @ColumnDefault("0")
     @Column(name = "thoiGianRanhTuan", columnDefinition = "tinyint")
     private Short thoiGianRanhTuan;
 
+    @ColumnDefault("0")
     @Column(name = "thoiGianRanhNgay", columnDefinition = "tinyint")
     private Short thoiGianRanhNgay;
 

52729349f20e2475aee7df0c2669c1d254cdd444
HPhi1808 <hoangphi120705@gmail.com>
2025-04-14 00:17:28 +0700

    thêm thông báo khi tham gia dự án 123

--- src/main/java/org/example/uthteammatching/services/ProjectService.java
+++ src/main/java/org/example/uthteammatching/services/ProjectService.java
@@ -21,17 +21,14 @@ public class ProjectService {
         return projectRepository.findByMaProject(maProject);
     }
 
-<<<<<<< HEAD
     public List<Project> searchProjects(String keyword) {
         if (keyword == null || keyword.trim().isEmpty()) {
             return projectRepository.findAll(); // Hoặc trả về danh sách rỗng
         }
-        return projectRepository.findByTenProjectContainingIgnoreCaseOrMoTaContainingIgnoreCase(keyword);
+        return projectRepository.findByTenProjectContainingIgnoreCase(keyword);
     }
 
-=======
     public boolean existsByTenProject(String nameProject) {
         return projectRepository.existsByTenProject(nameProject);
     }
->>>>>>> 597f62c63893779ae4208e50b7d418fd2d500b08
 }
